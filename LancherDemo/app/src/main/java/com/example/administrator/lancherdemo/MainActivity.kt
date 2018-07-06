baf601afb8eb436d95db61d520dca7a1bf6484cb
wangtwoyou <wq1308226780@live.com>
2018-07-06 17:59:35 +0800

    rxandroid测试了解一波，边缘ob

--- LancherDemo/app/src/main/java/com/example/administrator/lancherdemo/MainActivity.kt
+++ LancherDemo/app/src/main/java/com/example/administrator/lancherdemo/MainActivity.kt
@@ -2,16 +2,53 @@ package com.example.administrator.lancherdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import AppUtil
+import android.util.Log
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (AppUtil(this).getApps().size==0)
-            tv.text = "shibai"
-        else
-            tv.text = AppUtil(this).getApps()[0]
+        /*val applist = AppUtil(this).getApps()
+        Observable.just(applist)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.newThread())
+                .subscribe{
+                    tv.append(it.toString())
+                }*/
+        /*Thread(Runnable {
+            tv.text="123"
+        }).start()*/
+        Log.i("activity","onCreate()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("activity","onResume()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("activity","onStart()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("activity","onRestart()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("activity","onPause()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("activity","onDestroy()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("activity","onStop()")
     }
 }
