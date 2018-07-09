0d6eb6ea0983a95a565dbfc7e7d94590198b2b3d
wangtwoyou <wq1308226780@live.com>
2018-07-09 13:48:11 +0800

    使用rxjava做了一个输入并实时显示变化的功能，延时加time()

--- LancherDemo/app/src/main/java/com/example/administrator/lancherdemo/MainActivity.kt
+++ LancherDemo/app/src/main/java/com/example/administrator/lancherdemo/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.administrator.lancherdemo
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var str = "666"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,10 +26,42 @@ class MainActivity : AppCompatActivity() {
         /*Thread(Runnable {
             tv.text="123"
         }).start()*/
+        btn.setOnClickListener {
+            str = "888"
+        }
+        /*Observable.just(str)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(Schedulers.newThread())
+                .subscribe {
+                    tv.text = str
+                }*/
         Log.i("activity","onCreate()")
     }
 
     override fun onResume() {
+        super.onResume()
+        et.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Observable.just(p0)
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe {
+                    tv.text = p0
+                }
+            }
+        })
+
+    }
+    /*override fun onResume() {
         super.onResume()
         Log.i("activity","onResume()")
     }
@@ -50,5 +89,6 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         Log.i("activity","onStop()")
-    }
+    }*/
+
 }
